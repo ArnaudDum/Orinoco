771f2527eea57654be38c4de98a768475eb186bb
Arnaud Dumoncel <arnaud.dumoncel@gmail.com>
2021-01-11 15:25:57 +0100

    correctif sur le vide-cache

--- confirm.js
+++ confirm.js
@@ -2,15 +2,10 @@
 
 
 let showOrderID = document.getElementById('orderID');
-async function getReturnedID() {
+function getReturnedID() {
     let response = JSON.parse(localStorage.getItem('Recap-commande'));
     let retourID = response.orderId;
     return retourID;
 }
 
 showOrderID.innerHTML = getReturnedID();
-
-async function emptyCache() {
-    await getReturnedID();
-    localStorage.removeItem('Recap-commande');
-}
