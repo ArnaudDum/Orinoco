8a10d72e4d116a9ab70ca405fe91937e970d1ecb
Arnaud Dumoncel <arnaud.dumoncel@gmail.com>
2021-01-11 15:21:14 +0100

    vidage du localstorage après confirmation

--- confirm.js
+++ confirm.js
@@ -2,10 +2,15 @@
 
 
 let showOrderID = document.getElementById('orderID');
-function getReturnedID() {
+async function getReturnedID() {
     let response = JSON.parse(localStorage.getItem('Recap-commande'));
     let retourID = response.orderId;
     return retourID;
 }
 
 showOrderID.innerHTML = getReturnedID();
+
+async function emptyCache() {
+    await getReturnedID();
+    localStorage.removeItem('Recap-commande');
+}
